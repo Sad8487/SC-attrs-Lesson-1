4c31d3f10b37a0dcafccc1915cc6f7ac47329cc8
SvetlanaDyablo <sad8487@mail.ru>
2023-05-15 22:26:09 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import './App.css';
+import styled from 'styled-components';
+
+
+function App() {
+    return (
+        <div className="App">
+            <Title>Styled-components <span>.attrs</span> method</Title>
+
+            <Form>
+                <Field/>
+                <Field/>
+                <Field/>
+            </Form>
+        </div>
+    );
+}
+
+export default App;
+
+const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    margin-bottom: 32px;
+    span {
+      color: #e91e63;
+    }
+`;
+
+const Form = styled.form`
+  width: 100%;
+  max-width: 500px;
+`;
+
+const Field = styled.input`
+  padding: 5px 15px;
+  margin: 10px 0;
+  width: 100%;
+  font-size: 1rem;
+`;
